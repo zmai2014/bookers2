199dae6f2446d9951ceda1434939754289da4be0
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-07-15 10:45:47 +0000

    make contorollers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  resources :books, only: [:new, :index, :show]
+  get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
