4f5800812fee19164e5d6205e69848482600e0dc
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-07-20 09:40:25 +0000

    finish header.footer.top.about.login

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,7 @@ const { environment } = require('@rails/webpacker')
 module.exports = environment
 const webpack = require('webpack')
 environment.plugins.prepend(
-  'Provide'
+  'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
