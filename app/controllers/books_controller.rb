838f20601bb0b18eb158da6f5a662896da37070c
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-07-25 09:32:13 +0000

    flashmessage and validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,8 +20,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :books_path
+    end
   end
   
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
   
   def get_profile_image(width, height)
     unless profile_image.attached?
